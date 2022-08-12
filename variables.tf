85a87fcb4e915035449d09b308d37d0aade8bd3b
siddiquetic <90234933+siddiquetic@users.noreply.github.com>
2022-08-12 17:39:55 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIARLGGPM6KIF2BYTNK"
+}
+variable "secret_key" {
+  default = "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"
+}
+variable "region" {
+  default = "us-east-1"
+}
